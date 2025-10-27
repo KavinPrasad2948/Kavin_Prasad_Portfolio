7bd5bf4a6ecfc36ead3333afdae6e9ade9391270
K Kavin Prasad <86555433+KavinPrasad2948@users.noreply.github.com>
2025-10-27 22:45:05 +0100

    Update Hero.jsx update resume

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -34,7 +34,7 @@ const Hero = () => {
             </Button>
           </Link>
           <Link
-            to ="https://drive.google.com/file/d/1Z3I_QS0ATQJtW9McejRcUekOCA9r54op/view?usp=drive_link"
+            to ="https://drive.google.com/file/d/1JqsMoG6Xb2oApifk0RmsssRWyOQOKgk6/view?usp=drivesdk"
             target="_blank"
           >
             <Button
